535fa147c2021e0ef616772d55226bfe27472d66
AlirezaParchami <alirezaprm76@gmail.com>
2016-12-26 19:55:43 +0330

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MSN on 12/26/2016.
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("1.Add a Node \n 2.Delete a Node and keep its child \n 3.Delete a Node with its child \n 4.Edit Tag");
+
+    }
+}
