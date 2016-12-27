cf789f97ada6223f0002eff5a4cc366b44bc83b9
Mahdi <mahdi.salmanzadeh.najafi@gmail.com>
2016-12-27 13:23:27 +0330

    Tree.ToString and FileIO tested ...

--- src/main.java
+++ src/main.java
@@ -1,16 +1,16 @@
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by MSN on 12/26/2016.
  */
 public class main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IOException {
         //System.out.println("1.Add a Node \n 2.Delete a Node and keep its child \n 3.Delete a Node with its child \n 4.Edit Tag");
 
         TagNode root = new TagNode(null,null,null,null,null);
-        StringSpliter.split(FileReader.readHTML(),root);
-        System.out.println("_____________________________");
-        System.out.println(root.getChildren().get(0).toString(0));
+        StringSpliter.split(FileIO.read(),root);
+        if(root.getChildren()!=null || root.getChildren().get(0)!=null)
+            FileIO.write(root.getChildren().get(0).toString(0));
     }
 }
